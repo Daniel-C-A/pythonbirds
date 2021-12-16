6738d4036a2d70d9fc8c23f9caf0443493d288b1
Daniel Cruz Antunes <69701938+Daniel-C-A@users.noreply.github.com>
2021-12-16 19:57:33 -0300

    Filhos.nome

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=45):
+    def __init__(self, *filhos, nome=None, idade=45):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Antunes')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Daniel'
-    print(p.nome)
-    print(p.idade)
+    daniel = Pessoa(nome='Daniel')
+    antunes = Pessoa(daniel, nome='Antunes')
+    print(Pessoa.cumprimentar(antunes))
+    print(id(antunes))
+    print(antunes.cumprimentar())
+    print(antunes.nome)
+    print(antunes.idade)
+    for filho in antunes.filhos:
+        print(filho.nome)
